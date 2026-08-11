9d2e7bec3a88bcba329d6cc2a24829c2d4da1715
babla <barbara.qwp@gmail.com>
2026-08-11 13:17:40 -0300

    versão_talvez3

--- app/src/main/java/com/example/abluble_pdmii/tela02.java
+++ app/src/main/java/com/example/abluble_pdmii/tela02.java
@@ -1,15 +1,29 @@
 package com.example.abluble_pdmii;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class tela02 extends AppCompatActivity {
+public class tela02 extends AppCompatActivity implements MediaPlayer.OnCompletionListener, SeekBar.OnSeekBarChangeListener, Runnable {
 
+    private Toolbar toolbar;
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekbar;
+    private Handler handler;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +34,85 @@ public class tela02 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        toolbar = findViewById(R.id.toolbar);
+        //atribui a toolbar o poder de actionbar
+        setSupportActionBar(toolbar);
+        //habilita o botão de voltar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        seekbar = findViewById(R.id.seekBar);
+        seekbar.setOnSeekBarChangeListener(this);
+        handler =  new Handler();
+
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        if (id == R.id.id001){
+            if(mediaPlayer== null){
+                mediaPlayer = MediaPlayer.create(this, R.raw.forrodofarol_quincasmoreira);
+                mediaPlayer.setOnCompletionListener(this);
+                seekbar.setMax(mediaPlayer.getDuration());
+                handler.post(this);
+                mediaPlayer.start();
+            }else if(!mediaPlayer.isPlaying()){
+                mediaPlayer.start();
+            }
+        }
+        if(id == R.id.id003){
+            if(mediaPlayer != null){
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+        }
+        if(id == R.id.id002){
+            if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                mediaPlayer.pause();
+            }
+        }
+        return false;
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mediaPlayer.release();
+        mediaPlayer = null;
+        seekbar.setProgress(0);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        if(mediaPlayer != null){
+            mediaPlayer.seekTo(seekBar.getProgress());
+        }
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void run() {
+        if(mediaPlayer!= null){
+            seekbar.setProgress(mediaPlayer.getCurrentPosition());
+            handler.postDelayed(this,1000);
+        }
+
     }
 }
